936a584abf36a14d7926c0eb3b06c2928cbe3f09
Erik <erik.wiklund@duva.se>
2023-09-30 10:30:40 +0200

    Add link with seed who created

--- database/seeders/ThreadSeeder.php
+++ database/seeders/ThreadSeeder.php
@@ -14,22 +14,26 @@ class ThreadSeeder extends Seeder
     public function run(): void
     {
         Thread::create([
+            'created_by' => 1,
             'title' => 'Thread Title 1',
             'content' => 'Thread content for the first thread.',
             'sub_category_id' => 1, // Replace with the actual subcategory ID
         ]);
         Thread::create([
+            'created_by' => 1,
             'title' => 'Thread Title 3',
             'content' => 'Thread content for the first thread.',
             'sub_category_id' => 1, // Replace with the actual subcategory ID
         ]);
         Thread::create([
+            'created_by' => 2,
             'title' => 'Thread Title 4',
             'content' => 'Thread content for the first thread.',
             'sub_category_id' => 1, // Replace with the actual subcategory ID
         ]);
 
         Thread::create([
+            'created_by' => 3,
             'title' => 'Thread Title 2',
             'content' => 'Thread content for the second thread.',
             'sub_category_id' => 2, // Replace with the actual subcategory ID
