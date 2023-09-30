d4763e167a15e247557a1dd268dd9b91b893c25b
Erik <erik.wiklund@duva.se>
2023-09-30 14:51:08 +0200

    Add userid to model

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -15,9 +15,9 @@ class Thread extends Model
     }
 
     public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+{
+    return $this->belongsTo(User::class, 'user_id');
+}
 
     public function lastPoster()
 {
