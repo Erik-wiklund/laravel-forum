61944f03c891bf7bdf97199063a235eab3443199
Erik <erik.wiklund@duva.se>
2023-09-30 10:30:13 +0200

    Place userseed at top, fixing error with seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             CategorySeeder::class,
             SubCategorySeeder::class,
             ThreadSeeder::class,
+            
             // Add more seeders if needed
         ]);
     }
