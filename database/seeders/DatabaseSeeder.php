6e130a8261fee6e3807f51df2dc546cc49da285e
Erik <erik.wiklund@duva.se>
2023-10-03 11:16:51 +0200

    Add seeder to data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             SubCategorySeeder::class,
             ThreadSeeder::class,
+            RoleSeeder::class,
             
             // Add more seeders if needed
         ]);
