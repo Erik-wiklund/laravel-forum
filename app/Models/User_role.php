310721949acb321dfd6fe1001aed9b3f2766e877
Erik <erik.wiklund@duva.se>
2023-10-03 11:16:14 +0200

    Add realtion on user to role

---
+++ app/Models/User_role.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User_role extends Model
+{
+    use HasFactory;
+
+    public function users()
+    {
+        $this->hasMany(users::class);
+    }
+}
