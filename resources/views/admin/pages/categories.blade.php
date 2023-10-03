c025c9a775113d5cbab80ddd5b15e6911a0710b1
Erik <erik.wiklund@duva.se>
2023-10-03 10:50:13 +0200

    Add category and user pages in admin

--- resources/views/admin/pages/categories.blade.php
+++ resources/views/admin/pages/categories.blade.php
@@ -8,10 +8,10 @@
               <!--overview start-->
           <div class="row">
             <div class="col-lg-12">
-              <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
+              <h3 class="page-header"><i class="fa fa-laptop"></i> Forum Categories</h3>
               <ol class="breadcrumb">
                 <li><i class="fa fa-home"></i><a href="/dashboard/home">Home</a></li>
-                <li><i class="fa fa-users"></i>Users</li>
+                <li><i class="fa fa-users"></i>Categories</li>
               </ol>
             </div>
           </div>
@@ -21,7 +21,7 @@
             <div class="col-lg-12 col-md-12">
               <div class="panel panel-default">
                 <div class="panel-heading">
-                  <h2><i class="fa fa-flag-o red"></i><strong>Registered Users</strong></h2>
+                  <h2><i class="fa fa-flag-o red"></i><strong>Forum Categories</strong></h2>
                   <div class="panel-actions">
                     <a href="/dashboard/home" class="btn-setting"><i class="fa fa-rotate-right"></i></a>
                     <a href="/dashboard/home" class="btn-minimize"><i class="fa fa-chevron-up"></i></a>
@@ -32,9 +32,10 @@
                   <table class="table bootstrap-datatable countries">
                     <thead>
                       <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>User Role</th>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Order</th>
+                        <th>Description</th>
                         <th>View</th>
                         <th>Edit</th>
                         <th>Delete</th>
@@ -42,13 +43,14 @@
                       </tr>
                     </thead>
                     <tbody>
-                        @if (count($users)> 0)
-                            @foreach ($users as $user)
+                        @if (count($categories)> 0)
+                            @foreach ($categories as $category)
                             <tr>
-                                <td>{{$user->name}}</td>
-                                <td>{{$user->email}}</td>
-                                <td>{{$user->user_role}}</td>
-                                <td><a href="/dashboard/users/{{$user->id}}"><i class="fa fa-eye text-success"></i></a></td>
+                                <td>{{$category->title}}</td>
+                                <td>{{$category->image}}</td>
+                                <td>{{$category->order}}</td>
+                                <td>{{$category->desc}}</td>
+                                <td><a href="/dashboard/users/{{$category->id}}"><i class="fa fa-eye text-success"></i></a></td>
                                 <td><a href="#"><i class="fa fa-edit text-info"></i></a></td>
                                 <td><a href="#" class="text-danger"><i class="fa fa-trash"></i>Delete</a></td>
                                
@@ -58,7 +60,7 @@
                     </tbody>
                   </table>
 
-                  {{ $users->links() }}
+                  
                 </div>
   
               </div>
