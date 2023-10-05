3303f76dcc2ddd5de5636f47170ba36285934efc
Erik <erik.wiklund@duva.se>
2023-10-05 13:34:09 +0200

    Image resize when creating/editing categoryimage

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Intervention\Image\Facades\Image;
 use App\Models\Category;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
@@ -28,33 +29,37 @@ class CategoryController extends Controller
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        $request->validate([
-            'title' => 'required',
-            'desc' => 'required',
-            'order' => 'required',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-        ]);
+{
+    $request->validate([
+        'title' => 'required',
+        'desc' => 'required',
+        'order' => 'required',
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    ]);
 
-        $category = new Category;
-        $category->title = $request->title;
-        $category->desc = $request->desc;
-        $category->order = $request->order;
-        $category->user_id = auth()->id();
+    $category = new Category;
+    $category->title = $request->title;
+    $category->desc = $request->desc;
+    $category->order = $request->order;
+    $category->user_id = auth()->id();
 
-        // Generate a unique filename for the uploaded image
-        $imageFileName = time() . '.' . $request->image->extension();
+    // Generate a unique filename for the uploaded image
+    $imageFileName = time() . '.' . $request->image->extension();
 
-        // Move the uploaded image to the public/images folder with the generated filename
-        $request->image->move(public_path('images'), $imageFileName);
+    // Move the uploaded image to the public/images folder with the generated filename
+    $imagePath = public_path('images/' . $imageFileName);
+    $request->image->move(public_path('images'), $imageFileName);
 
-        $category->image = $imageFileName;
-        $category->save();
+    // Open the uploaded image using Intervention Image and resize it to fit within a 300x300 pixel box
+    Image::make($imagePath)->fit(300, 300)->save($imagePath);
 
-        Session::flash('message', 'Category created successfully');
-        Session::flash('alert-class', 'alert-success');
-        return back();
-    }
+    $category->image = $imageFileName;
+    $category->save();
+
+    Session::flash('message', 'Category created successfully');
+    Session::flash('alert-class', 'alert-success');
+    return back();
+}
 
 
     /**
@@ -78,39 +83,47 @@ class CategoryController extends Controller
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $categoryId)
-    {
-        $image='';
-        $imageFileName = '';
-        $new_imageFileName = '';
+{
+    $image = '';
+    $imageFileName = '';
+    $newImageFileName = '';
 
-        if ($request->image) {
-            $image = $request->image;
-            $imageFileName = $image->getClientOriginalName();
-            $new_imageFileName = time() . $imageFileName;
-            $request->image->move(public_path('images'), $new_imageFileName);
-        }
+    if ($request->image) {
+        $image = $request->image;
+        $imageFileName = $image->getClientOriginalName();
+        $newImageFileName = time() . $imageFileName;
 
-        $category = Category::find($categoryId);
-        if ($request->title) {
-            $category->title = $request->title;
-        }
-        if ($request->desc) {
-            $category->desc = $request->desc;
-        }
-        if ($request->order) {
-            $category->order = $request->order;
-        }
+        // Move the uploaded image to the public/images folder with the new filename
+        $imagePath = public_path('images/' . $newImageFileName);
+        $request->image->move(public_path('images'), $newImageFileName);
 
-        if ($request->image) {
-            $category->image = $new_imageFileName;
-        }
-        
-
-        $category->save();
-        Session::flash('message', 'Category updated successfully');
-        Session::flash('alert-class', 'alert-success');
-        return back();
+        // Open the uploaded image using Intervention Image and resize it to fit within a 300x300 pixel box
+        Image::make($imagePath)->fit(300, 300)->save($imagePath);
     }
+
+    $category = Category::find($categoryId);
+
+    if ($request->filled('title')) {
+        $category->title = $request->title;
+    }
+    if ($request->filled('desc')) {
+        $category->desc = $request->desc;
+    }
+    if ($request->filled('order')) {
+        $category->order = $request->order;
+    }
+
+    if ($request->image) {
+        $category->image = $newImageFileName;
+    }
+
+    $category->save();
+
+    Session::flash('message', 'Category updated successfully');
+    Session::flash('alert-class', 'alert-success');
+
+    return back();
+}
 
     /**
      * Remove the specified resource from storage.
