631980caa277836e5e02e585ba699f482b8db3ab
Erik <erik.wiklund@duva.se>
2023-10-08 14:15:01 +0200

    user info

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div>
+            <img src="" alt="" style="max-width: 15px;">
+            <h1>User Profile: {{ $user->name }}</h1>
+            </span>
+        </div>
+        <!-- Display user information and other details here -->
+    </div>
+@endsection
