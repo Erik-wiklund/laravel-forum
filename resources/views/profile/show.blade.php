a146a00e210e22a52fabb8014bab757fabf03ca8
Erik <erik.wiklund@duva.se>
2023-10-08 14:14:22 +0200

    Delete unused file

--- resources/views/profile/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app_no_navigation')
-
-@section('content')
-    <div class="container">
-        <h1>User Profile: {{ $user->name }}</h1>
-        <!-- Display user information and other details here -->
-    </div>
-    
-@endsection
