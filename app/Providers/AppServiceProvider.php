6effaa04974b054e6fccb3f9f1f8c519f56b304e
Erik <erik.wiklund@duva.se>
2023-10-08 14:16:48 +0200

    Add userid on the navigation throughout the app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('layouts.navigation', function ($view) {
+            // Fetch the currently authenticated user or null if no user is logged in
+            $user = auth()->user();
+    
+            // Share the $user variable with the navigation view
+            $view->with('user', $user);
+        });
     }
 }
