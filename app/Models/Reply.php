294b13bcbc671665c6f536b91e9e84f60c5f3e64
Erik <erik.wiklund@duva.se>
2023-10-09 11:23:47 +0200

    Add relations

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['content', 'user_id', 'thread_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
