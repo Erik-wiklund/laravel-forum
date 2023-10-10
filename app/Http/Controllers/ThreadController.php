dd484c598fe5a561908ea7464e66894784932943
Erik <erik.wiklund@duva.se>
2023-10-10 14:29:38 +0200

    Fix code to follow url structure. Fix views count

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PageView;
 use App\Models\SubCategory;
 use App\Models\Thread;
+use App\Models\ThreadView;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
 
 class ThreadController extends Controller
 {
@@ -16,9 +20,10 @@ class ThreadController extends Controller
         // Retrieve all threads related to the provided subcategory
         $threads = Thread::where('sub_category_id', $subcategory->id)->get();
 
-        // Pass the subcategory and threads to the view
-        return view('forum.threads.show', compact('threads', 'subcategory'));
+        return view('forum.threads.index', compact('threads', 'subcategory'));
     }
+
+
 
 
     /**
@@ -28,52 +33,67 @@ class ThreadController extends Controller
     {
         // Assuming you retrieve the subcategory here, adjust this based on your actual logic
         $subcategory = SubCategory::find(1); // Replace 1 with the actual subcategory ID or your logic to fetch it
-    
+
         return view('forum.threads.create', compact('subcategory'));
     }
-    
-    
+
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request, SubCategory $subcategory)
-{
-    // Validate the form data
-    $request->validate([
-        'title' => 'required|string|max:255',
-        'content' => 'required|string',
-    ]);
+    {
+        // Validate the form data
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
 
-    // Create a new thread and associate it with the subcategory
-    $thread = new Thread();
-    $thread->title = $request->input('title');
-    $thread->content = $request->input('content');
-    $thread->sub_category_id = $subcategory->id; // Associate with the current subcategory
-    $thread->created_by = auth()->user()->id; // Set the user ID of the creator
-    $thread->last_poster_id = auth()->user()->id; // Set the initial last poster
-    $thread->save();
+        // Create a new thread and associate it with the subcategory
+        $thread = new Thread();
+        $thread->title = $request->input('title');
+        $thread->content = $request->input('content');
+        $thread->sub_category_id = $subcategory->id; // Associate with the current subcategory
+        $thread->created_by = auth()->user()->id; // Set the user ID of the creator
+        $thread->last_poster_id = auth()->user()->id; // Set the initial last poster
+        $thread->save();
 
-    // Redirect to the newly created thread or another appropriate page
-    return redirect()->route('subcategories.threads.index', ['subcategory' => $subcategory, 'thread' => $thread]);
-}
+        // Redirect to the newly created thread or another appropriate page
+        return redirect()->route('subcategories.threads.index', ['subcategory' => $subcategory, 'thread' => $thread]);
+    }
 
 
     /**
      * Display the specified resource.
      */
-    public function show(int $id)
+    
+
+    public function show(SubCategory $subcategory, Thread $thread, Request $request)
     {
-        // // Retrieve the thread using the provided integer ID
-        // $thread = Thread::find($id);
+        // Retrieve the thread by its ID
+        $thread = Thread::findOrFail($thread->id);
 
-        // if (!$thread) {
-        //     // Handle the case where the thread with the given ID is not found (e.g., show an error page)
-        //     abort(404); // You can customize the error handling as needed
-        // }
+        // Check if the user is logged in and has not exceeded the rate limit
+        if (Auth::check() && !RateLimiter::tooManyAttempts('view-thread:' . $request->user()->id, 1)) {
+            // Increment the view count for this thread
+            $thread->incrementViewCount();
 
-        // return view('forum.threads.thread-content.show', compact('thread'));
+            // Define the rate limit key and duration in minutes (e.g., 60 minutes)
+            $rateLimitKey = 'view-thread:' . $request->user()->id;
+            $rateLimitDuration = 60;
+
+            // Apply rate limiting
+            RateLimiter::hit($rateLimitKey, $rateLimitDuration);
+        }
+
+        $replies = $thread->replies;
+
+        return view('forum.threads.show', compact('request', 'thread', 'subcategory', 'replies'));
     }
+
+
+
 
     /**
      * Show the form for editing the specified resource.
