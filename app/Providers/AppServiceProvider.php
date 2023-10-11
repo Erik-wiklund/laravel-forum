e004e55557073da0ee025a94c98d671a6d9fcc65
Erik <erik.wiklund@duva.se>
2023-10-11 09:53:48 +0200

    Choose which bootstrap to edit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
             // Share the $user variable with the navigation view
             $view->with('user', $user);
         });
+
+        Paginator::useBootstrapFive();
     }
 }
