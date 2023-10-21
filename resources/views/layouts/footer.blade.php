0d1c60bba92fcacd8965b77dc41dfd5701b4e8b2
Erik <erik.wiklund@duva.se>
2023-10-21 13:47:17 +0200

    Add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm" style="width: 33%;">
+            <label class="font-medium">About Us</label>
+            <div class="aboutUsContainer">
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. In maiores repudiandae illo optio aliquid
+                    laudantium delectus quos magnam eaque nesciunt explicabo nulla sed, doloremque dolores earum sequi!
+                    Repellat, autem consectetur.
+                    Enim maiores veritatis soluta ipsum vero adipisci nihil repudiandae voluptate vel in eligendi,
+                    praesentium iusto consequatur omnis porro voluptas qui rerum nulla, unde at excepturi? Velit debitis
+                    incidunt provident nemo?</span>
+            </div>
+        </div>
+        <div class="col-sm" style="width: 33%;">
+            <label class="font-medium">Quick Links</label>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="{{ route('contact.create') }}">Contact</a></li>
+            </ul>
+        </div>
+        <div class="col-sm" style="width: 33%;">
+            <label class="font-medium">Other Stuff</label>
+        </div>
+    </div>
+</div>
