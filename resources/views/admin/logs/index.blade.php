baaaf755491e190d08ac1ffaa1b108f6ca7b2d25
Erik <erik.wiklund@duva.se>
2023-10-24 11:25:02 +0200

    Add view for logs

---
+++ resources/views/admin/logs/index.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper">
+
+            <!--overview start-->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
+                    <ol class="breadcrumb">
+                        <li><i class="fa fa-home"></i><a href="/dashboard/home">Home</a></li>
+                        <li><i class="fa fa-users"></i>Admin Log</li>
+                    </ol>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-12 col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h2><i class="fa fa-flag-o red"></i><strong>Admin Actions</strong></h2>
+                            <div class="panel-actions">
+                                <a href="/dashboard/home" class="btn-setting"><i class="fa fa-rotate-right"></i></a>
+                                <a href="/dashboard/home" class="btn-minimize"><i class="fa fa-chevron-up"></i></a>
+                                <a href="/dashboard/home" class="btn-close"><i class="fa fa-times"></i></a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <table class="table bootstrap-datatable countries">
+                                <thead>
+                                    <tr>
+                                        <th>Admin</th>
+                                        <th>Main role</th>
+                                        <th>Target</th>
+                                        <th>Action</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($adminLogs as $log)
+                                    <tr>
+                                        <td>{{ $log->admin->name }}</td> <!-- Access the user's name via the relationship -->
+                                        <td>{{ $log->admin->role->name }}</td>
+                                        <td>{{ $log->targetUser->name }}</td> <!-- Adjust this according to your user structure -->
+                                        <td>{{ $log->action }}</td>
+                                        <td>{{ $log->created_at }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+
+                            {{-- {{ $users->links() }} --}}
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <!--/col-->
+
+            </div>
+
+
+
+        </section>
+
+
+    </section>
+    <!--main content end-->
+@endsection
