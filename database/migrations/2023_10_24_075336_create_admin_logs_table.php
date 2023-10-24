88a7121de73698c16f1867b4dc74283c92a3b630
Erik <erik.wiklund@duva.se>
2023-10-24 11:25:11 +0200

    Add table for admin log

---
+++ database/migrations/2023_10_24_075336_create_admin_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('action');
+            $table->string('resource_type');
+            $table->unsignedBigInteger('resource_id');
+            $table->timestamps();
+
+            // Define foreign key constraint to link to the users table
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin_logs');
+    }
+};
