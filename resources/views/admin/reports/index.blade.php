603b5a3161eac09cc6aa27b9627455eff74cc771
Erik <erik.wiklund@duva.se>
2023-11-04 11:31:06 +0100

    Add display of report info

---
+++ resources/views/admin/reports/index.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <!--main content start-->
+    <section id="main-content">
+        <section class="wrapper">
+
+            <!--overview start-->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
+                    <ol class="breadcrumb">
+                        <li><i class="fa fa-home"></i><a href="/dashboard/home">Home</a></li>
+                        <li><i class="fa fa-users"></i>Admin Reports</li>
+                    </ol>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-12 col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h2><i class="fa fa-flag-o red"></i><strong>Reports</strong></h2>
+                            <div class="panel-actions">
+                                <a href="/dashboard/home" class="btn-setting"><i class="fa fa-rotate-right"></i></a>
+                                <a href="/dashboard/home" class="btn-minimize"><i class="fa fa-chevron-up"></i></a>
+                                <a href="/dashboard/home" class="btn-close"><i class="fa fa-times"></i></a>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <table class="table bootstrap-datatable countries">
+                                <thead>
+                                    <tr>
+                                        <th>Reporting User</th>
+                                        <th>Reported Thread</th>
+                                        <th>Reported Reply</th>
+                                        <th>Reason</th>
+                                        <th>Content reported</th>
+                                        <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($reports as $report)
+                                        <tr>
+                                            <td>{{ $report->user->name }}</td>
+                                            <td>{{ optional($report->thread)->title }}</td>
+                                            <td>{{ $report->reported_reply }}</td>
+                                            <td>{{ $report->reason }}</td>
+                                            <td>{{ $report->created_at }}</td>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+
+                            {{-- {{ $users->links() }} --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+    </section>
+    <!--main content end-->
+@endsection
