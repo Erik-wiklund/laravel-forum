9fae5e4ef66e9028948175f67d128d0b6c10f10e
Erik-wiklund <85eriwik@skola.boras.se>
2024-02-02 13:05:49 +0100

    Removed form tags for navbuttons

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,17 +10,9 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 items-center sm:flex">
-
-                    <form action="{{ route('forum.index') }}">
-                        <a class=" navButtons h-16 p-2" type="submit">
-                            {{ __('Home') }}</a>
-                    </form>
-
-
-                    <form action="">
-                        <a class="navButtons h-16 p-2" type="submit">{{ __('Resources') }}</a>
-                    </form>
-
+                    <a href="{{ route('forum.index') }}" class=" navButtons h-16 p-2" type="submit">
+                        {{ __('Home') }}</a>
+                    <a href="/resources" class="navButtons h-16 p-2" type="submit">{{ __('Resources') }}</a>
                 </div>
             </div>
             <!-- Settings Dropdown -->
