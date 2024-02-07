6af42eed39075798c0b4add419d51d28fcfb821b
Erik-wiklund <85eriwik@skola.boras.se>
2024-02-07 15:05:57 +0100

    Add model for respources

---
+++ app/Models/Resources.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resources extends Model
+{
+    use HasFactory;
+
+    protected $table = 'resources';
+
+    protected $fillable = [
+        'created_by',
+        'category',
+        'title',
+        'description',
+        'version',
+        'tag_line',
+        'format',
+        'url',
+        'image',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
