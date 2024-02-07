de2c2ec66c897fa69bfe41aae04af32839f56baf
Erik-wiklund <85eriwik@skola.boras.se>
2024-02-07 10:46:50 +0100

    Add resource table

---
+++ database/migrations/2024_02_07_093335_create_resource_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('category');
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('version', 8, 2); // Adjust precision and scale as needed
+            $table->string('tag_line');
+            $table->string('format'); // Column for storing file format
+            $table->string('url')->nullable(); // Column for additional URL, allowing NULL values
+            $table->string('image')->nullable(); // Column for storing image path, allowing NULL values
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resource');
+    }
+};
