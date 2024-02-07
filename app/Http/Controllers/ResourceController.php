816b3391c1bd57c27fb4a15bd4460c997e992a6c
Erik-wiklund <85eriwik@skola.boras.se>
2024-02-07 15:06:21 +0100

    Store method and index for resources

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Intervention\Image\Facades\Image;
+use App\Models\Resources;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ResourceController extends Controller
 {
@@ -11,7 +14,8 @@ class ResourceController extends Controller
      */
     public function index()
     {
-        return view('resources.index');
+        $resources = Resources::all();
+        return view('resources.index', compact('resources'));
     }
 
     public function chose_category()
@@ -32,8 +36,69 @@ class ResourceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'file' => 'required|file|mimes:zip,rar,7z,tar,txt,mp3,wav,ogg,avi,mpg,mpeg,mkv,iso,pdf,png,jpg,jpeg,jpe,gif,psd,tif,bsp,dem,vtf,vmt,cfg,ini,sp,py',
+        ]);
+        // dd($request->all());
+        // Validate the request data
+        // $request->validate([
+        //     'category' => 'required',
+        //     'title' => 'required',
+        //     'description' => 'required',
+        //     'version' => 'required',
+        //     'format' => 'required',
+        // ]);
+
+        $title = $request->input('title');
+        $description = $request->input('description');
+        $category = $request->input('category');
+        $version = $request->input('version');
+        $tag_line = $request->input('tag_line');
+        $url = $request->input('url');
+
+        // Create a new resource instance
+        $resource = new Resources();
+        $resource->created_by = auth()->user()->id;
+        $resource->category = empty($category) ? null : $category;
+        $resource->title = empty($title) ? null : $title;
+        $resource->description = empty($description) ? null : $description;
+        $resource->version = empty($version) ? null : $version;
+        $resource->tag_line = empty($tag_line) ? null : $tag_line;
+        $resource->format = empty($format) ? null : $format;
+        $resource->url = empty($url) ? null : $url;
+        if ($request->hasFile('format')) {
+            // An image was uploaded
+            // Generate a unique filename for the uploaded image
+            $resourceFileName = time() . '.' . $request->format->extension();
+
+            $request->format->move(public_path('public_resources/resources'), $resourceFileName);
+
+            $resource->format = $resourceFileName;
+        }
+        if ($request->hasFile('image')) {
+            // An image was uploaded
+            // Generate a unique filename for the uploaded image
+            $imageFileName = time() . '.' . $request->image->extension();
+
+            // Move the uploaded image to the public/images folder with the generated filename
+            $imagePath = public_path('public_resources/images' . $imageFileName);
+            $request->image->move(public_path('public_resources/images'), $imageFileName);
+
+            // Open the uploaded image using Intervention Image and resize it to fit within a 300x300 pixel box
+            Image::make($imagePath)->fit(300, 300)->save($imagePath);
+
+            $resource->image = $imageFileName;
+        }
+
+
+        // Save the resource
+        $resource->save();
+
+        // Redirect to a success page or perform any other actions
+        return redirect()->back()->with('success', 'Resource added successfully.');
     }
+
+
 
     /**
      * Display the specified resource.
