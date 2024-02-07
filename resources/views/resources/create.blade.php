81c715b8db451c68ecd7e65359f6fac05c932143
Erik-wiklund <85eriwik@skola.boras.se>
2024-02-07 15:04:56 +0100

    Resource creation form

--- resources/views/resources/create.blade.php
+++ resources/views/resources/create.blade.php
@@ -1,12 +1,7 @@
-<?php
-use Illuminate\Support\Facades\Session;
-?>
 @extends('layouts.app_no_chat')
 
 @section('content')
     <!--main content start-->
-
-
     <section id="main-content">
         <section class="wrapper">
             <div class="row">
@@ -23,26 +18,47 @@ use Illuminate\Support\Facades\Session;
                             @endforeach
                         @endif
                         @if (\Session::has('message'))
-                            <p class="alert
-      {{ Session::get('alert-class', 'alert-success') }}">
-                                {{ Session::get('message') }}</p>
+                            <p class="alert {{ Session::get('alert-class', 'alert-success') }}">
+                                {{ Session::get('message') }}
+                            </p>
                         @endif
-                        <form class="form-horizontal" method="POST" action="{{ route('category.store') }}"
+                        <form class="form-horizontal" method="POST" action="{{ route('resources.store') }}"
                             enctype="multipart/form-data">
                             @csrf
 
+
                             <div class="form-group">
-                                <label class="col-lg-2 control-label">resource Title</label>
+                                <label class="col-lg-2 control-label">Resource Title</label>
                                 <div class="col-lg-10">
-                                    <input name="title" class="form-control" value="" />
+                                    <input name="title" type="text" class="form-control" value="" />
                                 </div>
                             </div>
                             @error('title')
                                 <p class="alert alert-danger">{{ $message }}</p>
                             @enderror
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Category</label>
+                                <div class="col-lg-10">
+                                    <select name="category" class="form-control">
+                                        <option value="software">Software</option>
+                                        <option value="art_images">Art & Images</option>
+                                        <!-- Add more category options as needed -->
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Version</label>
+                                <div class="col-lg-10">
+                                    <input name="version" type="text" class="form-control" value="" />
+                                    <span>the version number of your upload, (1.0) for example</span>
+                                </div>
+                            </div>
+                            @error('version')
+                                <p class="alert alert-danger">{{ $message }}</p>
+                            @enderror
 
                             <div class="form-group">
-                                <label class="col-lg-2 control-label">resource Image</label>
+                                <label class="col-lg-2 control-label">Resource Image</label>
                                 <div class="col-lg-10">
                                     <input type="file" name="image" class="form-control" />
                                 </div>
@@ -50,17 +66,53 @@ use Illuminate\Support\Facades\Session;
                             @error('image')
                                 <p class="alert alert-danger">{{ $message }}</p>
                             @enderror
+
                             <div class="form-group">
                                 <label class="col-lg-2 control-label">Resource Description</label>
                                 <div class="col-lg-10">
-                                    <textarea name="desc" id="editor1" class="form-control" cols="30" rows="5"></textarea>
+                                    <textarea name="description" id="editor1" class="form-control" cols="30" rows="5"></textarea>
                                 </div>
                             </div>
-                            @error('desc')
+                            @error('description')
+                                <p class="alert alert-danger">{{ $message }}</p>
+                            @enderror
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Choose resource</label>
+                                <div class="col-lg-10">
+                                    <input type="file" name="format" class="form-control" />
+                                    <label for="">Accepted upload extensions:
+                                        zip, rar, 7z, tar, txt, mp3, wav, ogg, avi, mpg, mpeg, mkv, iso,
+                                        pdf, png, jpg, jpeg, jpe, gif, psd, tif, bsp, dem, vtf, vmt, cfg, ini, sp,
+                                        py</label>
+                                </div>
+                            </div>
+                            @error('format')
+                                <p class="alert alert-danger">{{ $message }}</p>
+                            @enderror
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Tag Line</label>
+                                <div class="col-lg-10">
+                                    <input name="tag_line" type="text" class="form-control" value="" />
+                                    <span>Provide a very brief, one-line description of your resource.</span>
+                                </div>
+                            </div>
+                            @error('tag_line')
                                 <p class="alert alert-danger">{{ $message }}</p>
                             @enderror
 
-
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">Additional information URL</label>
+                                <div class="col-lg-10">
+                                    <input name="url" type="url" class="form-control" value="" />
+                                    <span>
+                                        If you have your own web page containing a demo, extended description or support
+                                        services etc. for this resource, enter its URL here.
+                                    </span>
+                                </div>
+                            </div>
+                            @error('url')
+                                <p class="alert alert-danger">{{ $message }}</p>
+                            @enderror
 
                             <div class="form-group">
                                 <div class="col-lg-offset-2 col-lg-10">
@@ -72,10 +124,6 @@ use Illuminate\Support\Facades\Session;
                     </div>
                 </section>
             </div>
-
-
         </section>
     </section>
-
-
 @endsection
