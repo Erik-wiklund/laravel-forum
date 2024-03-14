b2f469e510c1fa02d512d7a1961d088534675f3f
Erik-wiklund <85eriwik@skola.boras.se>
2024-03-14 10:04:45 +0100

    Helper method tagging and clickable

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,23 @@ function parseBBCodeToHTML($message)
     // Parse BBCode tags and convert to HTML
     return preg_replace('/\[QUOTE=(.*?)\](.*?)\[\/QUOTE\]/is', '<div class="quoted-message">$1 said: $2</div>', $message);
 }
+
+if (!function_exists('processMessageContent')) {
+    function processMessageContent($content, $user)
+    {
+        // Regular expression to find mentioned users
+        $pattern = '/@(\w+)/';
+
+        // Replace mentioned usernames with clickable links
+        $content = preg_replace_callback($pattern, function ($matches) use ($user) {
+            $username = $matches[1];
+            $userModel = \App\Models\User::where('username', $username)->first();
+            if ($userModel) {
+                return '<a style="color:gray;" data-user-id="' . $userModel->id . '" href="#" class="openProfileModal">@' . $username . '</a>';
+            }
+            return $matches[0]; // Return the original username if user not found
+        }, $content);
+
+        return $content;
+    }
+}
