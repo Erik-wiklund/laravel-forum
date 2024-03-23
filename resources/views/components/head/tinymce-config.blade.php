aa10a12d957605a4cf0478ef09daef620f762608
Erik-wiklund <85eriwik@skola.boras.se>
2024-03-23 12:19:38 +0100

    Remove tinymce for testing

--- resources/views/components/head/tinymce-config.blade.php
+++ resources/views/components/head/tinymce-config.blade.php
@@ -1,15 +1,15 @@
-<script src="https://cdn.tiny.cloud/1/s74k5fnuxm0en3a0hbb7nwieipr308qfg23uwdwz5w6hm9v1/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script src="https://cdn.tiny.cloud/1/s74k5fnuxm0en3a0hbb7nwieipr308qfg23uwdwz5w6hm9v1/tinymce/5/tinymce.min.js"
+    referrerpolicy="origin"></script>
 <script>
-  tinymce.init({
-    selector: 'textarea#reply-textarea', // Replace this CSS selector to match the placeholder element for TinyMCE
-    plugins: 'code table lists',
-    toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
-  });
+    // tinymce.init({
+    //     selector: 'textarea#reply-textarea', // Replace this CSS selector to match the placeholder element for TinyMCE
+    //     plugins: 'code table lists',
+    //     toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+    // });
 
-  tinymce.init({
+    tinymce.init({
         selector: 'textarea#message',
         plugins: 'code table lists',
         toolbar: 'undo redo | formatselect| bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
     });
 </script>
-
